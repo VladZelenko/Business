1f5b4fa79fd84d8060b00b6e749d06a6a076be48
VladZelenko <Vlad6555@ukr.net>
2017-01-29 15:37:12 +0200

    stcky header

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
 // include('partials/resize-img.js');
+$(window).scroll(function(e){
+	var $el = $('.header');
+	var isPositionFixed = ($el.css('position') == 'fixed');
+	if ($(this).scrollTop() > 42 && !isPositionFixed){
+		$('.header').css({'position': 'fixed', 'top': '0px' , 'z-index' : '10'});
+	}
+	if ($(this).scrollTop() < 42 && isPositionFixed)
+	{
+		$('.header').css({'position': 'static', 'top': '0px'});
+	}
+});
